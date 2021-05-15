15c6ad2c38951554eaa8a068fec23d481ddebe4d
KHR <reen13@ajou.ac.kr>
2021-05-15 20:13:01 +0900

    debug mode off

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ class photo(Resource):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug = True, port=80)
+    app.run(host='0.0.0.0', port=80)
     # 테스트할때는 127.0.0.1, app.debug = True
