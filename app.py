5b2fea8023aeda58b67804d9f0194a15c2b99df4
KHR <reen13@ajou.ac.kr>
2021-05-17 00:51:27 +0900

    debug off

--- app.py
+++ app.py
@@ -258,5 +258,5 @@ class photo(Resource):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug = True, port=5000) # 배포는 5000
+    app.run(host='0.0.0.0', port=5000) # 배포는 5000
     # 테스트할때는 '127.0.0.1', debug = True,
